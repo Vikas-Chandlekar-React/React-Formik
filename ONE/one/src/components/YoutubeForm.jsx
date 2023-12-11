8cc45b97b0727b2ae886f5912f00ccb56bb5896d
WorkAccVikas <workaccvikas@gmail.com>
2023-12-11 14:12:17 +0530

    c3

--- ONE/one/src/components/YoutubeForm.jsx
+++ ONE/one/src/components/YoutubeForm.jsx
@@ -2,20 +2,47 @@ import { useFormik } from "formik";
 import React from "react";
 
 function YoutubeForm() {
-  const formik = useFormik({});
-  console.log(`🚀 ~ file: YoutubeForm.jsx:6 ~ YoutubeForm ~ formik:`, formik);
+  console.log("YoutubeForm Re-render");
+
+  // NOTE : Here in initialValues key must be same as textbox value of name attribute
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+  });
+  console.log(`🚀 ~ Form values : `, formik.values);
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+        />
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" name="email" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+        />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input
+          type="text"
+          id="channel"
+          name="channel"
+          value={formik.values.channel}
+          onChange={formik.handleChange}
+        />
 
         <button>Submit</button>
       </form>
