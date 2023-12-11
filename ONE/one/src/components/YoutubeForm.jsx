bccaee105157eaf79ca4ca16b82f5d73c89844ea
WorkAccVikas <workaccvikas@gmail.com>
2023-12-11 14:52:29 +0530

    c5

--- ONE/one/src/components/YoutubeForm.jsx
+++ ONE/one/src/components/YoutubeForm.jsx
@@ -1,19 +1,53 @@
 import { useFormik } from "formik";
 import React from "react";
 
+const initialValues = {
+  name: "",
+  email: "",
+  channel: "",
+};
+
+const onSubmit = (values) => {
+  console.log("Form Submitted! = ", values);
+};
+
+const validate = (values) => {
+  /** NOTE : This validate function satisfied 3 conditions
+   *  1. return object
+   *  2. The key in error object must be same as values object
+   *  3. The value of key in errors object must be string indicating what are error message
+   *    e.g errors.name = "This field is required"
+   */
+
+  // values.name values.email values.channel
+  const errors = {};
+  // errors.name errors.email and errors.channel
+
+  if (!values.name) {
+    errors.name = "Required";
+  }
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email format";
+  }
+
+  if (!values.channel) {
+    errors.channel = "Required";
+  }
+
+  return errors;
+};
+
 function YoutubeForm() {
   console.log("YoutubeForm Re-render");
 
   // NOTE : Here in initialValues key must be same as textbox value of name attribute
   const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      channel: "",
-    },
-    onSubmit: (values) => {
-      console.log("Form Submitted! = ", values);
-    },
+    initialValues,
+    onSubmit,
+    validate,
   });
   // console.log(`🚀 ~ Form values : `, formik.values);
 
