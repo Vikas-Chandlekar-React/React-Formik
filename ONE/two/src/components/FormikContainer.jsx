a3f9d46f5cef87119f33a0030a97abdea8a91dfa
WorkAccVikas <workaccvikas@gmail.com>
2023-12-14 15:22:05 +0530

    c29

--- ONE/two/src/components/FormikContainer.jsx
+++ ONE/two/src/components/FormikContainer.jsx
@@ -10,16 +10,24 @@ const dropdownOptions = [
   { key: "Option 3", value: "option 3" },
 ];
 
+const radioOptions = [
+  { key: "Option 1", value: "roption1" },
+  { key: "Option 2", value: "roption2" },
+  { key: "Option 3", value: "roption3" },
+];
+
 const initialValues = {
   email: "",
   description: "",
   selectOption: "",
+  radioOption: "",
 };
 
 const validationSchema = Yup.object({
   email: Yup.string().required("Required!"),
   description: Yup.string().required("Required!"),
   selectOption: Yup.string().required("Required!"),
+  radioOption: Yup.string().required("Required!"),
 });
 
 const onSubmit = (values) => {
@@ -55,6 +63,13 @@ function FormikContainer() {
               label="Select a topic"
               name="selectOption"
               options={dropdownOptions}
+            />
+
+            <FormikControl
+              control="radio"
+              label="Radio Option"
+              name="radioOption"
+              options={radioOptions}
             />
 
             <button type="submit">Submit</button>
