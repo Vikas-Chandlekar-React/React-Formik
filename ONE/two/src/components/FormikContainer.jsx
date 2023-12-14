503c6237475708c7d384660f1a82eb79657cd6a6
WorkAccVikas <workaccvikas@gmail.com>
2023-12-14 14:38:16 +0530

    c28

--- ONE/two/src/components/FormikContainer.jsx
+++ ONE/two/src/components/FormikContainer.jsx
@@ -3,14 +3,23 @@ import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import FormikControl from "./FormikControl";
 
+const dropdownOptions = [
+  { key: "Select an option", value: "" },
+  { key: "Option 1", value: "option1" },
+  { key: "Option 2", value: "option2" },
+  { key: "Option 3", value: "option 3" },
+];
+
 const initialValues = {
   email: "",
   description: "",
+  selectOption: "",
 };
 
 const validationSchema = Yup.object({
   email: Yup.string().required("Required!"),
   description: Yup.string().required("Required!"),
+  selectOption: Yup.string().required("Required!"),
 });
 
 const onSubmit = (values) => {
@@ -34,11 +43,20 @@ function FormikContainer() {
               label="Email"
               name="email"
             />
+
             <FormikControl
               control="textarea"
               label="Description"
               name="description"
             />
+
+            <FormikControl
+              control="select"
+              label="Select a topic"
+              name="selectOption"
+              options={dropdownOptions}
+            />
+
             <button type="submit">Submit</button>
           </Form>
         );
