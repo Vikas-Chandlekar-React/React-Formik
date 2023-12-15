d3e9b9a059eb385a97ba2cc9db4caf25db97628d
WorkAccVikas <workaccvikas@gmail.com>
2023-12-15 22:08:03 +0530

    c32

---
+++ ONE/two/src/components/LoginForm.jsx
@@ -0,0 +1,53 @@
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
+import FormikControl from "./FormikControl";
+
+const initialValues = {
+  email: "",
+  password: "",
+};
+
+const validationSchema = Yup.object({
+  email: Yup.string().email("Invalid email format").required("Required!"),
+  password: Yup.string().required("Required!"),
+});
+
+const onSubmit = (values) => {
+  console.log("Form submitted = ", values);
+};
+
+function LoginForm() {
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {(formik) => {
+        return (
+          <Form>
+            <FormikControl
+              control="input"
+              type="email"
+              name="email"
+              label="Email"
+            />
+
+            <FormikControl
+              control="input"
+              type="password"
+              name="password"
+              label="Password"
+            />
+
+            <button type="submit" disabled={!formik.isValid}>
+              Submit
+            </button>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+}
+
+export default LoginForm;
