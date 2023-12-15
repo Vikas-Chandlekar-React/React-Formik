10bb32cf5d3a05f67b617338ac619fe2dca8cb21
WorkAccVikas <workaccvikas@gmail.com>
2023-12-16 02:18:04 +0530

    c35

--- ONE/two/src/App.jsx
+++ ONE/two/src/App.jsx
@@ -6,14 +6,18 @@ import LoginForm from "./components/LoginForm";
 import RegistrationForm from "./components/RegistrationForm";
 import EnrollmentForm from "./components/EnrollmentForm";
 
+import { ChakraProvider, theme as chakraTheme } from "@chakra-ui/react";
+
 function App() {
   return (
-    <div className="App">
-      {/* <FormikContainer /> */}
-      {/* <LoginForm /> */}
-      {/* <RegistrationForm /> */}
-      <EnrollmentForm />
-    </div>
+    <ChakraProvider theme={chakraTheme}>
+      <div className="App">
+        {/* <FormikContainer /> */}
+        {/* <LoginForm /> */}
+        {/* <RegistrationForm /> */}
+        <EnrollmentForm />
+      </div>
+    </ChakraProvider>
   );
 }
 
